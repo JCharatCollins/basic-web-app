5db7b4df91f280fbbab88996030841713c533cea
Gabriel Hall <adrastopoulos@gmail.com>
2024-03-14 16:01:15 -0400

    feat: name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -11,7 +11,8 @@ export default function QueryProcessor(query: string): string {
     return "Pob.";
   }
 
-  const numbers = "Which of the following numbers is the largest:"
+  // Regex for which of the following numbers is the largest
+  const numbers = "which of the following numbers is the largest: ";
 
   if (query.toLowerCase().includes(numbers.toLowerCase())) {
     const sub = query.toLowerCase().substring(numbers.length, query.length);
@@ -19,6 +20,17 @@ export default function QueryProcessor(query: string): string {
     const numArr = numList.map((num) => parseInt(num));
     const max = Math.max(...numArr);
     return max.toString();
+  }
+
+  // Regex for what is x + y
+  const add = /what is (\d+) plus (\d+)/;
+  if (add.test(query)) {
+    const match = query.match(add);
+    if (match) {
+      const x = parseInt(match[1]);
+      const y = parseInt(match[2]);
+      return (x + y).toString();
+    }
   }
 
   return "";
